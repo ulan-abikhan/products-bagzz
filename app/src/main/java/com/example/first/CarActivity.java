f55d240dcfb4c93a4d73a09249f5b3dc5d0168d5
ulan-abikhan <uabikhan@gmail.com>
2023-05-19 20:04:53 +0600

    bottom sheet dialog with ok was added

--- app/src/main/java/com/example/first/CarActivity.java
+++ app/src/main/java/com/example/first/CarActivity.java
@@ -5,11 +5,15 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.first.fragment.FragmentPagerAdapter;
 import com.example.first.model.Product;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.squareup.picasso.Picasso;
@@ -22,6 +26,11 @@ public class CarActivity extends AppCompatActivity {
 
     TextView productTitle;
 
+    Button buyNow;
+
+    BottomSheetDialog bottomSheetDialog;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +38,10 @@ public class CarActivity extends AppCompatActivity {
 
         thumbnail = findViewById(R.id.product_thumbnail);
         productTitle = findViewById(R.id.productTitle);
+        buyNow = findViewById(R.id.product_buy_button);
+
+        bottomSheetDialog();
+        onBuyButtonClick();
 
 
         Product product = getIntent().getParcelableExtra("product");
@@ -48,4 +61,26 @@ public class CarActivity extends AppCompatActivity {
 
 
     }
+
+    private void bottomSheetDialog() {
+        View view = getLayoutInflater().inflate(R.layout.buy_bottom_sheet_dialog, null);
+
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setCancelable(true);
+        LayoutInflater layoutInflater = bottomSheetDialog.getLayoutInflater();
+        bottomSheetDialog.setContentView(view);
+
+        Button okBottomSheet = view.findViewById(R.id.ok_bottom_sheet);
+        okBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.dismiss();
+            }
+        });
+    }
+
+    private void onBuyButtonClick() {
+        buyNow.setOnClickListener(v -> bottomSheetDialog.show());
+    }
+
 }
